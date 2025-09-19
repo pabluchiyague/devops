a987bea18c66e62fda8cc6667eb188c335f89b4e
Pablo Yague Carrasco <pabluchiyague@gmail.com>
2025-09-19 17:07:06 +0100

    Added Hello World

---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,11 @@
+package com.napier.sem;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Boo yah!");
+
+    }
+}
